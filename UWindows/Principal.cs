76cabc6ec9f5c9fed32a0545ea8b87c4ff99429a
Michael Melato <maiconmelatto@gmail.com>
2025-03-24 21:44:53 -0400

    Renomeado formulário Principal

--- UWindows/Principal.cs
+++ UWindows/Principal.cs
@@ -1,8 +1,8 @@
 namespace UWindows
 {
-    public partial class Form1 : Form
+    public partial class Principal : Form
     {
-        public Form1()
+        public Principal()
         {
             InitializeComponent();
         }
